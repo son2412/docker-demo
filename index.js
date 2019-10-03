5bf2ca00e639bd25f2d5346656b0078be9f2b9bb
son2412 <ngovanson196@gmail.com>
2019-10-03 15:33:09 +0700

    mongo

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const app = require("express")();
 const MongoClient = require("mongodb").MongoClient;
 require('dotenv').config();
 
-// MongoClient.connect("mongodb://localhost:27017").then(connection => {
-//   console.log(connection);
-// });
-
-MongoClient.connect(process.env.DB_URL, function(err, client) {
-  console.log("Connected successfully to server");
-
-  const db = client.db(process.env.DB_NAME);
-  findDocuments(db, function() {
-    client.close();
-  });
+MongoClient.connect(process.env.DB_URL).then(connection => {
+  console.log(connection);
 });
+
+// MongoClient.connect(process.env.DB_URL, function(err, client) {
+//   console.log("Connected successfully to server");
+
+//   const db = client.db(process.env.DB_NAME);
+//   findDocuments(db, function() {
+//     client.close();
+//   });
+// });
 
 app.listen(3000, () => console.log("Server is running"));
 
